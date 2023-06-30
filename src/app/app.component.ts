adcff3d896e4acd1f408bb538255885feda0de48
Andrew <neebet.2015@yandex.ru>
2023-06-30 04:45:26 +0300

    Little adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,34 +6,56 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  inputValue= '';
-  firstColor= 'gray';
-  secondColor = 'gray';
-  thirdColor = 'gray';
+  inputValue = '';
+  firstColor = 'grey';
+  secondColor = 'grey';
+  thirdColor = 'grey';
   handleInputChange(): void {
+    this.setDefaultColor();
+    if (this.inputValue.length > 8) {
+      if (this.hard()) {
+        this.setColorStrong();  //Strong Password
+      } else if (this.medium()) {
+        this.setColorMedium();  //Medium Password
+      } else {
+        this.setColorHard(); //Weak Password
+      }
+    }
+  }
+
+
+  setDefaultColor() {
     this.firstColor= 'gray';
     this.secondColor = 'gray';
     this.thirdColor = 'gray';
+  }
 
-    if (this.inputValue.length > 8) {
-      if (this.hasSymbols() &&
-        this.hasDigits() &&
-        this.hasLetters()) {
-        this.firstColor = 'green';
-        this.secondColor = 'green';
-        this.thirdColor = 'green'; //Strong Password
-      } else if (this.hasLetters() && this.hasSymbols() ||
-        this.hasLetters() && this.hasDigits() ||
-        this.hasDigits() && this.hasSymbols()) {
-        this.firstColor = 'yellow';
-        this.secondColor = 'yellow';
-        this.thirdColor = 'gray'; //Medium Password
-      } else {
-        this.firstColor = 'red';
-        this.secondColor = 'gray';
-        this.thirdColor = 'gray'; //Weak Password
-      }
-    }
+  setColorStrong() {
+    this.firstColor = 'green';
+    this.secondColor = 'green';
+    this.thirdColor = 'green';
+  }
+
+  setColorMedium() {
+    this.firstColor = 'yellow';
+    this.secondColor = 'yellow';
+    this.thirdColor = 'gray';
+  }
+
+  setColorHard() {
+    this.firstColor = 'red';
+    this.secondColor = 'gray';
+    this.thirdColor = 'gray';
+  }
+  medium(): boolean {
+    return this.hasLetters() && this.hasSymbols() ||
+    this.hasLetters() && this.hasDigits() ||
+    this.hasDigits() && this.hasSymbols()
+  }
+  hard(): boolean {
+    return this.hasSymbols() &&
+    this.hasDigits() &&
+    this.hasLetters()
   }
 
   hasSymbols(): boolean  {
@@ -46,49 +67,4 @@ export class AppComponent {
   hasLetters(): boolean {
     return /[a-zA-Z]/.test(this.inputValue)
   }
-
-
-  // updateColor(): void {
-  //   if (this.inputValue.length === 2) {
-  //     this.textColor = 'blue';
-  //   } else if (this.inputValue.length > 6) {
-  //     this.textColor = 'yellow';
-  //   } else if (this.inputValue.length > 8) {
-  //     this.textColor = 'green';
-  //   }
-  // }
-
-  // myForm: FormGroup;
-  // passwordStrength= '';
-  //
-  // constructor(private formBuilder: FormBuilder) {
-  //   this.myForm = this.formBuilder.group({
-  //     password: ['', [Validators.required, Validators.minLength(8)]]
-  //   });
-  // }
-  //
-  //   const passwordControl = this.myForm.get('password');
-  //   if (passwordControl && passwordControl.value !== null) {
-  //     const password = passwordControl.value;
-  //     if (password.length === 0) {
-  //       this.passwordStrength = '';
-  //     } else if (password.length < 8) {
-  //       this.textColor: = red;
-  //       this.style.color = 'black';
-  //     } else if (/[a-zA-Z]/.password && /[0-9]/.password && /[!@#$%^&*(),.?":{}|<>]/.password) {
-  //       this.style.color = 'black';
-  //     } else {
-  //       this.style.color = 'black';
-  //     }
-  //   }
-  //
-  // onSubmit() {
-  //   if (this.myForm.valid) {
-  //     // Form is valid, perform form submission
-  //     console.log(this.myForm.value);
-  //   } else {
-  //     // Form is invalid, display error messages or take appropriate action
-  //     console.log('Form is invalid.');
-  //   }
-  // }
 }
