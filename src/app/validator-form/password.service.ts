6aab2bd36d149416a670d1996311e6afdcf55176
Andrew <neebet.2015@yandex.ru>
2023-07-01 17:02:13 +0300

    last adjustments

---
+++ src/app/validator-form/password.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class PasswordService {
+  constructor() { }
+
+  validatePassword(passwordValue: string): string[] {
+    // Variables of validation based on complexity
+    const hasLetters = /[a-zA-Z]/.test(passwordValue);
+    const hasNumbers = /\d/.test(passwordValue);
+    const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(passwordValue);
+
+    // Logical variables for current state of passwordValue
+    const isBlank: boolean = passwordValue.length === 0;
+    const isNotZero: boolean = passwordValue.length !== 0;
+    const isLess8chars: boolean = passwordValue.length < 8;
+
+    // ComplexityLevel
+    const complexityLevel = (hasLetters ? 1 : 0) + (hasNumbers ? 1 : 0) + (hasSymbols ? 1 : 0);
+
+    if (isBlank) {
+      return ['gray', 'gray', 'gray'];
+
+    } else if (isLess8chars && isNotZero){
+      return ['red', 'red', 'red'];
+
+    } else {
+
+      // Receiving numbers for level strength estimating
+      switch (complexityLevel) {
+
+        // Weak level
+        case 1:
+          return ['red', 'gray', 'gray'];
+
+        // Medium level
+        case 2:
+          return ['yellow', 'yellow', 'gray'];
+
+        // Strong level
+        case 3:
+          return ['green', 'green', 'green'];
+
+        default:
+          return ['gray', 'gray', 'gray'];
+
+      }
+    }
+  }
+}
+
--- src/app/validator-form/validator-form.component.ts
+++ src/app/validator-form/validator-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { PasswordService } from './password.service';
 
 @Component({
     selector: 'app-validator-form',
@@ -17,11 +18,12 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 export class ValidationPasswordFormComponent implements ControlValueAccessor {
 
     passwordValue: string = '';
-
     element: string[] = ['gray', 'gray', 'gray'];
 
     onChange = (_: any):void => {};
     onTouched = ():void => {};
+
+    constructor(private passwordService: PasswordService) {}
 
     writeValue(value: string): void {
         this.passwordValue = value;
@@ -37,60 +39,8 @@ export class ValidationPasswordFormComponent implements ControlValueAccessor {
     }
 
     passwordValidation() {
-
-        // Variables of validation based on complexity
-        const hasLetters = /[a-zA-Z]/.test(this.passwordValue);
-        const hasNumbers = /\d/.test(this.passwordValue);
-        const hasSymbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(this.passwordValue);
-
-        // Logical variables for current state of passwordValue
-        const isBlank: boolean = this.passwordValue.length === 0;
-        const isNotZero: boolean = this.passwordValue.length !== 0;
-        const isLess8chars: boolean = this.passwordValue.length < 8;
-
-        // ComplexityLevel
-        const complexityLevel = (hasLetters ? 1 : 0) + (hasNumbers ? 1 : 0) + (hasSymbols ? 1 : 0);
-
-        if (isBlank) {
-
-          this.element = ['gray', 'gray', 'gray'];
-          this.onChange('');
-
-          return;
-        } else if (isLess8chars && isNotZero){
-
-          this.element = ['red', 'red', 'red'];
-          this.onChange('');
-
-          return;
-        } else {
-
-          // Receiving numbers for level strength estimating
-          switch (complexityLevel) {
-
-            // Weak level
-            case 1:
-
-              this.element = ['red', 'gray', 'gray'];
-              this.onChange(this.passwordValue);
-              return;
-
-            // Medium level
-            case 2:
-
-              this.element = ['yellow', 'yellow', 'gray'];
-              this.onChange(this.passwordValue);
-              return;
-
-            // Strong level
-            case 3:
-
-              this.element = ['green', 'green', 'green'];
-              this.onChange(this.passwordValue);
-              return;
-          }
-        }
-
+      this.element = this.passwordService.validatePassword(this.passwordValue);
+      this.onChange(this.passwordValue);
       }
       // On every change calling function
       onPasswordChange(event: any): void {
